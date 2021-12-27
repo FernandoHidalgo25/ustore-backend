9c9c3e9a489c3f9280bc17d7d2ad0670455fb64a
github username <github email address>
2021-12-26 23:11:03 -0500

    update controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,10 @@
 class SessionsController < ApplicationController
+    def create
+        user = User.find_by_username(params[:username])
+        if user && user.authenticate(params[:password])
+          render json: {user: UserSerializer.new(user), token: encode_token(user.id)}
+        else
+          render json: {errors: "Invalid Username and/or Password, get good!"}, status: :forbidden
+        end
+      end
 end
