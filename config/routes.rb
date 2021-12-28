fdffe4da7a1d6c82d5162544a11c964304a14a83
github username <github email address>
2021-12-28 00:20:33 -0500

    submit comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
   resources :users, only: [:create]
-  resources :stores, only: [:index, :show]
+  resources :stores, only: [:index, :show] do
+    resources :comments, only: [:create]
+  end
   resources :sessions, only: [:create]
   get "/me", to: 'sessions#autologin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
